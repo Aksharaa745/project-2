0039611d5801d1b2a1b14f6273c540b9872c3c80
Aksharaa745 <aksharaab73@gmail.com>
2025-06-19 15:27:35 +0530

    complete functions topic

--- functions.js
+++ functions.js
@@ -27,3 +27,36 @@ const arrowFuncVar = ()=>{
 };
 
 arrowFuncVar();
+
+//Filter , Map, Reduce
+let nums = [1,2,3,4,5];
+let evens = nums.filter((n)=>{
+    return (n%2===0);
+});
+let newEvents = nums.filter((n) => n%2 === 0);
+console.log(evens);
+
+let newList = [];
+
+for(let i = 0;i<nums.length;i++){
+    if(nums[i]%2 === 0){
+        newList.push(nums[i]);
+    }
+}
+console.log(newList);
+
+console.log("Using Map method");
+let squares = nums.map((n) => {
+    return n*n;
+});
+console.log(squares);
+
+
+//Reduce method
+console.log("Using Reduce method");
+//let sum = nums.reduce((sum, n) => sum + n,0);
+let newSum = nums.reduce((sum, n) =>{
+    return sum + n;
+}, (sum = 0));
+console.log(newSum);
+
